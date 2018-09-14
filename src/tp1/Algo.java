46a77fc0256b909dab6ab8de242b7bbaa6d82a46
Tim Lepage <timlepage9@gmail.com>
2018-09-14 14:54:43 +0200

    started

--- src/tp1/Algo.java
+++ src/tp1/Algo.java
@@ -1,5 +1,59 @@
 package tp1;
 
 public class Algo {
+	public int arraySize = 4;
+	public float[] arr1;
+	public float[] arr2;
+
+	public Algo() {
+		arr1 = new float[] { 1, 2, 3, 4};
+		arr2 = new float[arraySize];
+	}
+
+	public float[] decomposition(float[] arr1, float[] arr2, int size) {
+		int j = 0;
+		for (int i = 0; i < 2 * size; i += 2) {
+			arr2[j] = (arr1[i] + arr1[i + 1]) / 2;
+			arr2[j + size] = arr2[j] - arr1[i];
+			j++;
+		}
+		return arr2;
+	}
+
+	public float[] recomposition(float[] arr1, float[] arr2, int size) {
+		int j = 0;
+		for (int i = 0; i < size; i += 2) {
+			arr2[j] = arr1[i] - arr1[i + size];
+			arr2[j + 1] = arr1[i] + arr1[i + size];
+			j += 2;
+		}
+		return arr2;
+	}
+	
+	public float[] decomplete(float[] arr1, float[] arr2, int size){
+		while(size > 1){
+			size = size/2;
+			arr1 = decomposition(arr1, arr2, size);
+			for (int i = 0; i < arr1.length; i++) {
+				System.out.println(arr1[i]);
+			}
+			System.out.println("\n");
+		}
+		return arr1;
+	}
+
+	/**
+	 * @require origin.lenght = end.lenght
+	 * @param origin
+	 * @param end
+	 * @return
+	 */
+	public double distance(float[] origin, float[] end) {
+		float sum = 0;
+		for (int i = 0; i < origin.length; i++) {
+			sum += Math.pow(origin[i] - end[i], 2);
+		}
+		return Math.sqrt(sum);
+	}
 
 }
--- src/tp1/Main.java
+++ src/tp1/Main.java
@@ -1,5 +1,21 @@
 package tp1;
 
 public class Main {
+	public static void main(String[] args) {
+		Algo algo1 = new Algo();
+		// algo1.decomposition(algo1.arr1, algo1.arr2, 2);
+		// for(int i = 0; i < algo1.arr2.length; i ++){
+		// System.out.println(algo1.arr2[i]);
+		// }
+		// System.out.println("\n\n");
+		// algo1.recomposition(algo1.arr2, algo1.arr1, 2);
+		// for(int i = 0; i < algo1.arr1.length; i ++){
+		// System.out.println(algo1.arr1[i]);
+		// }
 
+		algo1.decomplete(algo1.arr1, algo1.arr2, 4);
+		for (int i = 0; i < algo1.arr1.length; i++) {
+			System.out.println(algo1.arr1[i]);
+		}
+	}
 }
