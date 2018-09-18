def34600eacf43a38a94c3a0c2135b9c7307e678
Tim Lepage <timlepage9@gmail.com>
2018-09-18 13:25:06 +0200

    Compression ok

--- src/tp1/Main.java
+++ src/tp1/Main.java
@@ -1,8 +1,10 @@
 package tp1;
 
 public class Main {
+
 	public static void main(String[] args) {
-		float epsilon = (float) 1.5;
+		boolean debug = false;
+		float epsilon = (float) 0.1;
 		Algo algo1 = new Algo();
 		// algo1.decomposition(algo1.arr1, algo1.arr2, 2);
 		// for(int i = 0; i < algo1.arr2.length; i ++){
@@ -14,25 +16,30 @@ public class Main {
 		// System.out.println(algo1.arr1[i]);
 		// }
 
-		float[] res = algo1.decomplete(algo1.arr1, algo1.arraySize);
-		System.out.println("Tableau décomposé\n");
-		for (int i = 0; i < algo1.arr1.length; i++) {
-			System.out.println(res[i]);
+		double[] res = algo1.decomplete(algo1.arr1, algo1.arraySize);
+		if (debug) {
+			System.out.println("Tableau décomposé\n");
+			for (int i = 0; i < algo1.arr1.length; i++) {
+				System.out.println(res[i]);
+			}
 		}
-		
-		float[] compressed = algo1.compression(res, epsilon);
-		System.out.println("\nTableau compressé\n");
-		for (int i = 0; i < algo1.arr1.length; i++) {
-			System.out.println(compressed[i]);
+
+		double[] compressed = algo1.compression(res, epsilon);
+		if (debug) {
+			System.out.println("\nTableau compressé\n");
+			for (int i = 0; i < algo1.arr1.length; i++) {
+				System.out.println(compressed[i]);
+			}
 		}
-		
-		float[] res2 = algo1.recomplete(compressed, 1);
-		System.out.println("\nResultat recomposition\n");
-		for (int i = 0; i < algo1.arr1.length; i++) {
-			System.out.println(res2[i]);
+
+		double[] res2 = algo1.recomplete(compressed, 1);
+		if (debug) {
+			System.out.println("\nResultat recomposition\n");
+			for (int i = 0; i < algo1.arr1.length; i++) {
+				System.out.println(res2[i]);
+			}
 		}
-		
-		System.out.println("\nDistance entre les tableaux "+ algo1.distance(res2, algo1.arrbase));
-		
+		System.out.println("\nDistance entre les tableaux " + algo1.distance(res2, algo1.arrbase));
+
 	}
 }
